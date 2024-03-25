abbdc53c5dc9cbe8269f372b58fb69002f3a413d
NabilaFerdousPrapty <128033656+NabilaFerdousPrapty@users.noreply.github.com>
2024-03-25 17:02:12 +0600

    edits

--- src/components/DonationPage.jsx
+++ src/components/DonationPage.jsx
@@ -1,22 +1,23 @@
 
 
 const DonationPage = ({allDonation}) => {
-     const {picture,category,description,category_bg,card_bg,text_button_bg,title}=allDonation;
-    return (
-        <div className="">
-          <div className="h-64 max-w-lg p-6 rounded-md shadow-md flex gap-6  justify-between items-center" style={{backgroundColor:card_bg}}>
-        <img src={picture} alt="" className=" object-center  rounded-md w-2/4 " />
-        <div className="mt-6 mb-2">
-            <span className=" text-xs font-medium tracking-widest uppercase p-2 rounded-2xl" style={{ backgroundColor: category_bg }}>{category}</span>
-            <h2 className="text-3xl">{title}</h2>
-            <button className="btn btn-primary">View Details</button>
-            
-        </div>
-       
-    </div>
-    
-        </div>
-    );
+    const {picture,category,description,category_bg,card_bg,text_button_bg,title,price}=allDonation;
+   return (
+       <div className="">
+         <div className="h-64 max-w-lg p-6 rounded-md shadow-md flex gap-6  justify-between items-center" style={{backgroundColor:card_bg}}>
+       <img src={picture} alt="" className=" object-center  rounded-md w-2/4 " />
+       <div className="mt-6 mb-2">
+           <span className=" text-xs font-medium tracking-widest uppercase p-2 rounded-2xl" style={{ backgroundColor: category_bg }}>{category}</span>
+           <h2 className="text-3xl">{title}</h2>
+           <h4 className="text-xl font-bold " style={{color:text_button_bg}}  >tk:{price}</h4>
+           <button className="btn rounded-xl" style={{backgroundColor:text_button_bg, opacity:.7}}>View Details</button>
+           
+       </div>
+      
+   </div>
+   
+       </div>
+   );
 };
 
 export default DonationPage;
